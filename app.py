713bce7b480a8b2047839e2887a06a96a906d6ec
pnHoan <hiiamhoan@gmail.com>
2023-04-23 14:03:26 +0700

    clean file and fix user hadler

--- app.py
+++ app.py
@@ -28,12 +28,12 @@ appsecret_proof = hmac.new(
 ).hexdigest()
 
 ##server
-server = db.Server("Store","templateReq")
 
 def get_bot_response(message):
     """This is just a dummy function, returning a variation of what
     the user said. Replace this function with one connected to chatbot."""
     #return "This is a dummy response to '{}'".format(message)
+    server = db.Server("Store","templateReq","req")
     ressult = server.semanticSearch(message,["case_no"])
     return "Your request is in {}".format(ressult[0][0])
 
