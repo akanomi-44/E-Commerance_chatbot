53bd15b576af9ee22296104578e23f8b62dc320b
Phan Ngoc Hoan <hiiamhoan@gmail.com>
2023-05-28 17:05:22 +0700

    add handle for status code for send_webhook_message

--- handlers/requestHandler.py
+++ handlers/requestHandler.py
@@ -25,16 +25,35 @@ def handle_default(message, field):
     else:
         return f"I can only process request that is in following cases:\n\n 1. Recommendation: Can you recommend some [product] for [event] ?,...  \n\n 2. Make an order: Where can i order this [item],... \n\n 3. Contact human assistant: Contact the manager,... \n\n 4. Answer related questions: What is the fashion trend of the 90s ?,How do I properly care for and store my leather jackets ?,...  \n\n 5. Get shop's location: Where can i find your shop,..."
 
-async def send_webhook_message( type, message, user_id, url):
+async def send_webhook_message(type, message, user_id, url):
     payload = {
         "user_id": user_id,
-        "type":type,
+        "type": type,
         "message": message
     }
     headers = {
-            'Content-Type': 'application/json'
-        }
-    
+        'Content-Type': 'application/json'
+    }
+
     async with httpx.AsyncClient() as client:
-        response= await client.post(url, headers=headers,json=payload)
-        return response.json()
+        try:
+            response = await client.post(url, headers=headers, json=payload)
+            if response.status_code == 200:
+                print("Success: Request was successful.")
+                return response.json()
+
+            elif response.status_code == 400:
+                print("Bad Request: The server could not process the request.")
+
+            elif response.status_code == 404:
+                print("Not Found: The requested resource was not found.")
+
+            elif response.status_code == 500:
+                print("Internal Server Error: An error occurred on the server.")
+            else:
+                print(f"Unexpected Status Code: {response.status_code}")
+
+            return   
+        except httpx.RequestError as exc:
+            print(f"An error occurred while sending the webhook: {exc}")
+            return
